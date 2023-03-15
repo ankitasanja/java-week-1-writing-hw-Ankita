1c2b4fe34c47e289ac97727530773e2b74a1dae7
ankitasanja <ankitasanja11@gmail.com>
2023-03-15 22:23:14 +0000

    My homework week 6

---
+++ src/writinghomeworkweek1/Program_3_PrintInitials.java
@@ -0,0 +1,21 @@
+package writinghomeworkweek1;
+
+/**
+ * 3.Write a program that will print your initials to standard output in letters that are
+ * nine lines tall. Each big letter should be made up of a bunch of *'s. for example, if
+ * your initials were "JAJA", then the output would look something like:
+ */
+public class Program_3_PrintInitials {
+    public static void main(String[] args) {
+        System.out.println("*********  ********  *********  ********");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   ********        **   ********");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("   ****    **    **     ****    **    **");
+        System.out.println("    **     **    **      **     **    **");
+    }
+}
+
---
+++ src/writinghomeworkweek1/Programme_6_AmericanFlag.java
@@ -0,0 +1,24 @@
+package writinghomeworkweek1;
+
+/**
+ * 6. Write a Java program to print an american flag on the screen.
+ */
+public class Programme_6_AmericanFlag {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+    }
+}
